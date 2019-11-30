8e3df85bb7c99cd73bfc88b697391bc78946e07d
Laurene <laurenegicquel@gmail.com>
2019-11-30 08:43:47 +0100

    jkjkj

---
+++ src/test/avengers/PostTest.java
@@ -0,0 +1,64 @@
+package avengers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PostTest {
+
+	@Test
+	public void testGetContent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetContent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetReplyPid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetReplyPid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetReplyUid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetReplyUid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
